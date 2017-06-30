a252367e7d38b95a82ef1d637a34740b8787fce2
imkkc <240264735@qq.com>
2017-06-30 17:50:23 +0800

    +注释自定义函数库的加载规范

--- app/imkkcHelpers.php
+++ app/imkkcHelpers.php
@@ -1,7 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: hm
- * Date: 17/6/30
- * Time: 下午4:55
+ * 自定义方法库
+ *
+ * 在composer.json中 找到autoload 追加
+ *
+ *   "files": [
+ *       "app/imkkcHelpers.php"
+ *   ],
+ *
+ * 运行命令 composer dump-autoload 任何位置都可以使用这里方法，就像laravel的dd()、abort()
+ *
  */
+
+if(!function_exists('kkc')){
+    function kkc(){
+        return 'this is test imkkcHelpers';
+    }
+}
