c089f3f948f8eeea267fff7d79ede22476decdd8
imkkc <240264735@qq.com>
2017-06-30 17:07:34 +0800

    +增加自定义公共方法库 用composer dump-autoload方式自动载入

---
+++ app/imkkcHelpers.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hm
+ * Date: 17/6/30
+ * Time: 下午4:55
+ */
--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -17,4 +17,5 @@ return array(
     '58571171fd5812e6e447dce228f52f4d' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
     'f18cc91337d49233e5754e93f3ed9ec3' => $vendorDir . '/laravelcollective/html/src/helpers.php',
     'f960e77410032f236cef8c56617b313e' => $vendorDir . '/overtrue/laravel-lang/src/helpers.php',
+    'f582bee52496a33ec38d52f41482fdd2' => $baseDir . '/app/imkkcHelpers.php',
 );
